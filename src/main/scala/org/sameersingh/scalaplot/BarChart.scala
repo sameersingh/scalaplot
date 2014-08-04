64fd0853281c05a75680297715713ea993bec906
Sameer Singh <sameeersingh@gmail.com>
2014-08-03 23:34:18 -0700

    bar data is just a seq of doubles, copy over xychart implicits

--- src/main/scala/org/sameersingh/scalaplot/BarChart.scala
+++ src/main/scala/org/sameersingh/scalaplot/BarChart.scala
@@ -32,12 +32,8 @@ abstract class BarSeries {
   var borderLineType: Option[LineType.Type] = None
 }
 
-class MemBarSeries(val xs: Seq[Int], val ys: Seq[Double], val name: String = "Label") extends BarSeries {
-  def this(points: Seq[(Int, Double)], name: String = "Label") = this(points.map(_._1), points.map(_._2), name)
-
-  assert(xs.length == ys.length)
-
-  def isLarge: Boolean = xs.length > 1000
+class MemBarSeries(val ys: Seq[Double], val name: String = "Label") extends BarSeries {
+  def isLarge: Boolean = ys.length > 1000
 
   def writeToFile(filename: String, names: Int => String) {
     val writer = new PrintWriter(filename)
@@ -46,6 +42,8 @@ class MemBarSeries(val xs: Seq[Int], val ys: Seq[Double], val name: String = "La
     writer.flush()
     writer.close()
   }
+
+  val xs = (0 until ys.length)
 
   def toStrings(names: Int => String): Seq[String] = (0 until xs.length).map(i => xs(i).toString + "\t" + ys(i).toString + "\t" + names(i))
 
@@ -70,3 +68,211 @@ class BarChart(chartTitle: Option[String], val data: BarData,
 
   def this(data: BarData) = this(None, data)
 }
+
+trait BarSeriesImplicits {
+
+  trait YPoints {
+    def xsToYs(xs: Seq[Double]): Seq[Double]
+  }
+
+  //  implicit def seqToYpoints(ys: Seq[Double]): YPoints = new YPoints {
+  //    override def xsToYs(xs: Seq[Double]): Seq[Double] = ys
+  //  }
+  //
+  //  implicit def funcToYpoints(f: Double => Double): YPoints = new YPoints {
+  //    override def xsToYs(xs: Seq[Double]): Seq[Double] = xs.map(f)
+  //  }
+
+  //  implicit def paraFuncToYpoints(f: (Double) => Double): YPoints = new YPoints {
+  //    override def xsToYs(xs: Seq[Double]): Seq[Double] = xs.map(x => f(x))
+  //  }
+  implicit def anyToOptionAny[A](a: A): Option[A] = Some(a)
+
+  abstract class Y(val label: String = "Label",
+                   val style: XYPlotStyle.Type = XYPlotStyle.LinesPoints,
+                   val color: Option[Color.Type] = None,
+                   val ps: Option[Double] = None,
+                   val pt: Option[PointType.Type] = None,
+                   val lw: Option[Double] = None,
+                   val lt: Option[LineType.Type] = None,
+                   val every: Option[Int] = None) extends YPoints
+
+  object Y {
+    def apply(yp: Seq[Double],
+              label: String = "Label",
+              style: XYPlotStyle.Type = XYPlotStyle.LinesPoints,
+              color: Option[Color.Type] = None,
+              ps: Option[Double] = None,
+              pt: Option[PointType.Type] = None,
+              lw: Option[Double] = None,
+              lt: Option[LineType.Type] = None,
+              every: Option[Int] = None): Y = new Y(label, style, color, ps, pt, lw, lt, every) {
+      override def xsToYs(xs: Seq[Double]): Seq[Double] = yp
+    }
+  }
+
+  object Yf {
+    def apply(f: Double => Double,
+              label: String = "Label",
+              style: XYPlotStyle.Type = XYPlotStyle.LinesPoints,
+              color: Option[Color.Type] = None,
+              ps: Option[Double] = None,
+              pt: Option[PointType.Type] = None,
+              lw: Option[Double] = None,
+              lt: Option[LineType.Type] = None,
+              every: Option[Int] = None): Y = new Y(label, style, color, ps, pt, lw, lt, every) {
+      override def xsToYs(xs: Seq[Double]): Seq[Double] = xs.map(f)
+    }
+  }
+
+  object XY {
+    def apply(points: Seq[(Double, Double)],
+              label: String = "Label",
+              style: XYPlotStyle.Type = XYPlotStyle.LinesPoints,
+              color: Option[Color.Type] = None,
+              ps: Option[Double] = None,
+              pt: Option[PointType.Type] = None,
+              lw: Option[Double] = None,
+              lt: Option[LineType.Type] = None,
+              every: Option[Int] = None): XYSeries = {
+      val s = new MemXYSeries(points, label)
+      s.color = color
+      s.plotStyle = style
+      s.pointSize = ps
+      s.pointType = pt
+      s.lineWidth = lw
+      s.lineType = lt
+      s.every = every
+      s
+    }
+  }
+
+  def series(xs: Seq[Double], y: Y): XYSeries = {
+    val s = new MemXYSeries(xs, y.xsToYs(xs), y.label)
+    s.color = y.color
+    s.plotStyle = y.style
+    s.pointSize = y.ps
+    s.pointType = y.pt
+    s.lineWidth = y.lw
+    s.lineType = y.lt
+    s.every = y.every
+    s
+  }
+
+  implicit def pairSeqToSeries(xys: Iterable[(Double, Double)]): XYSeries = new MemXYSeries(xys.toSeq)
+
+  implicit def seqPairToSeries(xy: Pair[Iterable[Double], Iterable[Double]]): XYSeries = new MemXYSeries(xy._1.toSeq, xy._2.toSeq)
+
+  implicit def seqFuncPairToSeries(xy: Pair[Iterable[Double], Double => Double]): XYSeries = new MemXYSeries(xy._1.toSeq, xy._1.map(xy._2).toSeq)
+
+  implicit def xyspectoSeries(xy: Pair[Seq[Double], Y]): XYSeries = series(xy._1, xy._2)
+}
+
+object BarSeriesImplicits extends BarSeriesImplicits
+
+trait BarDataImplicits extends BarSeriesImplicits {
+  implicit def seriesSeqToData(ss: Iterable[XYSeries]): XYData = new XYData(ss.toSeq: _*)
+
+  implicit def seriesToData(s: XYSeries): XYData = new XYData(s)
+
+  // implicit def seriesStarToData(ss: XYSeries*): XYData = new XYData(ss: _*)
+
+  // implicit def data(xys: Pair[Seq[Double], Y]): XYData = new XYData(series(xys._1, xys._2))
+
+  def Ys(ys: Iterable[Seq[Double]]): Seq[Y] = ys.map(y => Y(y)).toSeq
+
+  def Ys(ys: Seq[Double]*): Seq[Y] = ys.map(y => Y(y)).toSeq
+
+  // seq of double
+  implicit def xSeqYToData(xy: Pair[Seq[Double], Seq[Double]]): XYData = xy._1 -> Ys(xy._2)
+
+  implicit def xSeqY2ToData(xy: Pair[Seq[Double], Product2[Seq[Double], Seq[Double]]]): XYData = xy._1 -> Ys(xy._2._1, xy._2._2)
+
+  implicit def xSeqY3ToData(xy: Pair[Seq[Double], Product3[Seq[Double], Seq[Double], Seq[Double]]]): XYData = xy._1 -> Ys(xy._2._1, xy._2._2, xy._2._3)
+
+  implicit def xSeqY4ToData(xy: Pair[Seq[Double], Product4[Seq[Double], Seq[Double], Seq[Double], Seq[Double]]]): XYData = xy._1 -> Ys(xy._2._1, xy._2._2, xy._2._3, xy._2._4)
+
+  implicit def xSeqY5ToData(xy: Pair[Seq[Double], Product5[Seq[Double], Seq[Double], Seq[Double], Seq[Double], Seq[Double]]]): XYData = xy._1 -> Ys(xy._2._1, xy._2._2, xy._2._3, xy._2._4, xy._2._5)
+
+  implicit def xSeqY6ToData(xy: Pair[Seq[Double], Product6[Seq[Double], Seq[Double], Seq[Double], Seq[Double], Seq[Double], Seq[Double]]]): XYData = xy._1 -> Ys(xy._2._1, xy._2._2, xy._2._3, xy._2._4, xy._2._5, xy._2._6)
+
+  // seq of funcs
+  implicit def xFYToData(xy: Pair[Seq[Double], Double => Double]): XYData = xy._1 -> Yfs(xy._2)
+
+  implicit def xFY2ToData(xy: Pair[Seq[Double], Product2[Double => Double, Double => Double]]): XYData = xy._1 -> Yfs(xy._2._1, xy._2._2)
+
+  implicit def xFY3ToData(xy: Pair[Seq[Double], Product3[Double => Double, Double => Double, Double => Double]]): XYData = xy._1 -> Yfs(xy._2._1, xy._2._2, xy._2._3)
+
+  implicit def xFY4ToData(xy: Pair[Seq[Double], Product4[Double => Double, Double => Double, Double => Double, Double => Double]]): XYData = xy._1 -> Yfs(xy._2._1, xy._2._2, xy._2._3, xy._2._4)
+
+  implicit def xFY5ToData(xy: Pair[Seq[Double], Product5[Double => Double, Double => Double, Double => Double, Double => Double, Double => Double]]): XYData = xy._1 -> Yfs(xy._2._1, xy._2._2, xy._2._3, xy._2._4, xy._2._5)
+
+  implicit def xFY6ToData(xy: Pair[Seq[Double], Product6[Double => Double, Double => Double, Double => Double, Double => Double, Double => Double, Double => Double]]): XYData = xy._1 -> Yfs(xy._2._1, xy._2._2, xy._2._3, xy._2._4, xy._2._5, xy._2._6)
+
+  // seq of Ys
+  implicit def xYToData(xy: Pair[Seq[Double], Y]): XYData = xy._1 -> Seq(xy._2)
+
+  implicit def xY2ToData(xy: Pair[Seq[Double], Product2[Y, Y]]): XYData = xy._1 -> Seq(xy._2._1, xy._2._2)
+
+  implicit def xY3ToData(xy: Pair[Seq[Double], Product3[Y, Y, Y]]): XYData = xy._1 -> Seq(xy._2._1, xy._2._2, xy._2._3)
+
+  implicit def xY4ToData(xy: Pair[Seq[Double], Product4[Y, Y, Y, Y]]): XYData = xy._1 -> Seq(xy._2._1, xy._2._2, xy._2._3, xy._2._4)
+
+  implicit def xY5ToData(xy: Pair[Seq[Double], Product5[Y, Y, Y, Y, Y]]): XYData = xy._1 -> Seq(xy._2._1, xy._2._2, xy._2._3, xy._2._4, xy._2._5)
+
+  implicit def xY6ToData(xy: Pair[Seq[Double], Product6[Y, Y, Y, Y, Y, Y]]): XYData = xy._1 -> Seq(xy._2._1, xy._2._2, xy._2._3, xy._2._4, xy._2._5, xy._2._6)
+
+  type Func = Double => Double
+
+  def Yfs(fs: Iterable[Double => Double]): Seq[Y] = fs.map(f => Yf(f)).toSeq
+
+  def Yfs(fs: Func*): Seq[Y] = fs.map(f => Yf(f)).toSeq
+
+  implicit def xYsToData(xys: Pair[Seq[Double], Iterable[Y]]): XYData = new XYData(xys._2.map(y => series(xys._1, y)).toSeq: _*)
+
+  implicit def xYSeqToData(xys: Iterable[Pair[Seq[Double], Y]]): XYData = new XYData(xys.map(xy => series(xy._1, xy._2)).toSeq: _*)
+
+  implicit def data(xs: Seq[Double], ys: Y*): XYData = new XYData(ys.map(y => series(xs, y)).toSeq: _*)
+}
+
+object BarDataImplicits extends BarDataImplicits
+
+trait BarChartImplicits extends BarDataImplicits {
+  implicit def dataToChart(d: XYData): XYChart = new XYChart(d)
+
+  implicit def stringToOptionString(string: String): Option[String] = if (string.isEmpty) None else Some(string)
+
+  def Axis(label: String = "", backward: Boolean = false, log: Boolean = false,
+           range: Option[(Double, Double)] = None): NumericAxis = {
+    val a = new NumericAxis
+    a.label = label
+    if (backward) a.backward else a.forward
+    if (log) a.log else a.linear
+    range.foreach({
+      case (min, max) => a.range_=(min -> max)
+    })
+    a
+  }
+
+  def bar(data: XYData, title: String = "",
+          x: NumericAxis = new NumericAxis,
+          y: NumericAxis = new NumericAxis,
+          pointSize: Option[Double] = None,
+          legendPosX: LegendPosX.Type = LegendPosX.Right,
+          legendPosY: LegendPosY.Type = LegendPosY.Center,
+          showLegend: Boolean = false,
+          monochrome: Boolean = false,
+          size: Option[(Double, Double)] = None
+           ): XYChart = {
+    val c = new XYChart(stringToOptionString(title), data, x, y)
+    c.pointSize = pointSize
+    c.legendPosX = legendPosX
+    c.legendPosY = legendPosY
+    c.showLegend = showLegend
+    c.monochrome = monochrome
+    c.size = size
+    c
+  }
+}
+
+object BarChartImplicits extends BarChartImplicits
--- src/test/scala/org/sameersingh/scalaplot/BarPlotTest.scala
+++ src/test/scala/org/sameersingh/scalaplot/BarPlotTest.scala
@@ -22,7 +22,7 @@ class BarPlotTest {
 
   @Test
   def testGnuplotOneSmallColMem(): Unit = {
-    val series = new MemBarSeries(0 until 10, (0 until 10).map(i => ((i+1) * (i+1)).toDouble), "Series")
+    val series = new MemBarSeries((0 until 10).map(i => ((i+1) * (i+1)).toDouble), "Series")
     val data = new BarData((x:Int)=> "Label" + x, Seq(series))
     val chart = new BarChart("Chart", data)
     val plotter = new GnuplotPlotter(chart)
@@ -34,13 +34,13 @@ class BarPlotTest {
   @Test
   def testGnuplotTwoLargeColMem(): Unit = {
     val rand = new scala.util.Random(0)
-    val series1 = new MemBarSeries(0 until 10, (0 until 10).map(i => (rand.nextDouble())), "Series1") {
+    val series1 = new MemBarSeries((0 until 10).map(i => (rand.nextDouble())), "Series1") {
       override def isLarge = true
     }
     series1.color = Some(Style.Color.Purple)
     series1.fillStyle = Some(Style.FillStyle.Pattern)
     series1.pattern = Some(2)
-    val series2 = new MemBarSeries(0 until 10, (0 until 10).map(i => (rand.nextDouble())), "Series2") {
+    val series2 = new MemBarSeries((0 until 10).map(i => (rand.nextDouble())), "Series2") {
       override def isLarge = true
     }
     series2.fillStyle = Some(Style.FillStyle.Solid)
@@ -56,10 +56,10 @@ class BarPlotTest {
   @Test
   def testGnuplotTwoSmallColMem(): Unit = {
     val rand = new scala.util.Random(0)
-    val series1 = new MemBarSeries(0 until 10, (0 until 10).map(i => (rand.nextDouble())), "Series1") {
+    val series1 = new MemBarSeries((0 until 10).map(i => (rand.nextDouble())), "Series1") {
       override def isLarge = false
     }
-    val series2 = new MemBarSeries(0 until 10, (0 until 10).map(i => (rand.nextDouble())), "Series2") {
+    val series2 = new MemBarSeries((0 until 10).map(i => (rand.nextDouble())), "Series2") {
       override def isLarge = false
     }
     val data = new BarData((x:Int)=> "Label" + x, Seq(series1, series2))
